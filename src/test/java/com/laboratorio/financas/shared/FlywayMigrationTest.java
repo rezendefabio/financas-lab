a27447ec0683dc762d874da36bb86218a3def6de
Fabio Rezende de Andrade <rezendefabio@gmail.com>
2026-05-08 13:14:06 -0300

    test: adiciona FlywayMigrationTest validando aplicacao da V1

---
+++ src/test/java/com/laboratorio/financas/shared/FlywayMigrationTest.java
@@ -0,0 +1,39 @@
+package com.laboratorio.financas.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FlywayMigrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void deveTerAplicadoMigrationV1ComSucesso() {
+        // Given - Flyway aplicou as migrations no startup do contexto Spring
+
+        // When
+        Boolean v1Aplicada = jdbcTemplate.queryForObject(
+            "SELECT success FROM flyway_schema_history WHERE version = '1'",
+            Boolean.class
+        );
+
+        // Then
+        assertThat(v1Aplicada).isTrue();
+    }
+
+    @Test
+    void deveTerCriadoTabelaHealthcheckComLinhaPlaceholder() {
+        // When
+        Integer total = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM __healthcheck",
+            Integer.class
+        );
+
+        // Then
+        assertThat(total).isGreaterThanOrEqualTo(1);
+    }
+}
