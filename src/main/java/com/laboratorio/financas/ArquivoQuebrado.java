0d923ba4999747dd90c72f4de9170d1c1d9eeb28
Fabio Rezende de Andrade <rezendefabio@gmail.com>
2026-05-08 09:46:06 -0300

    test: branch protection deve bloquear este merge

---
+++ src/main/java/com/laboratorio/financas/ArquivoQuebrado.java
@@ -0,0 +1,8 @@
+﻿package com.laboratorio.financas;
+
+public class ArquivoQuebrado {
+    public void metodo() {
+        // Erro de sintaxe proposital - falta ponto e virgula
+        int x = 5
+    }
+}
